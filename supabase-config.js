28bfa35d96c4780a296d0c8a9d802d13c2bcd0cc
Pergantis <pergadis@gmail.com>
2026-05-13 23:23:55 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,1 +1,6 @@
 
+// supabase-config.js
+const SUPABASE_CONFIG = {
+    URL: "https://xsakmdmhpuqjebcqrony.supabase.co",
+    KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzYWttZG1ocHVxamViY3Fyb255Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMzAzNzYsImV4cCI6MjA5MzkwNjM3Nn0.E6s11F_894jwxfaQoE4rfSX8oPtgi1DSeYOsyON1DB4"
+};
